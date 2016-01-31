d127f87c4b92c33ee5971e24a3ca018a1336685e
cloudera <cloudera@quickstart.cloudera>
2016-01-30 16:13:12 -0800

    fixed bug in weblog.py

--- PS02/weblog.py
+++ PS02/weblog.py
@@ -25,27 +25,26 @@ class Weblog(object):
         self.ipaddr = m.group(1)
         self.timestamp = m.group(2)
         self.request = m.group(3)
-        self.result = m.group(4)
+        self.result = int(m.group(4))
         self.user = m.group(5)
-        self.agent = m.group(6)
-        self.referrer = m.group(7)
+        self.referrer = m.group(6)
+        self.agent = m.group(7).replace('"','')
         self.url = self.request.split(" ")[1]
         self.datetime = parser.parse(self.timestamp.replace(":", " ", 1)).isoformat()
         self.date = self.datetime[0:10]
         self.time = self.datetime[11:]
         
 # Tests
-# test with python -m unittest
+# test with pytest
 demo_line1 = '172.16.0.3 - - [25/Sep/2002:14:04:19 +0200] "GET /hello.html HTTP/1.1" 401 - "" "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.1) Gecko/20020827"'
-import unittest
-class TestWeblog(unittest.TestCase):
-    def test_parser(self):
-        obj = Weblog(demo_line1)
-        self.assertEqual(obj.ip,"172.16.0.3")
-        self.assertEqual(obj.timestamp,parser.parse("25-Sep-2002 14:14:19 +0200"))
-        self.assertEqual(obj.request,"GET /hello.html HTTP/1.1")
-        self.assertEqual(obj.result,401)
-        self.assertEqual(obj.agent,"Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.1) Gecko/20020827")
+def test_weblog():
+    obj = Weblog(demo_line1)
+    assert obj.ipaddr=="172.16.0.3"
+    assert obj.datetime==parser.parse("25-Sep-2002 14:04:19 +0200").isoformat()
+    assert obj.request=="GET /hello.html HTTP/1.1"
+    assert obj.result==401
+    assert obj.referrer==""
+    assert obj.agent=="Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.1) Gecko/20020827"
         
 
 # test the parser
