65524d624aaaba1034c3b027b77cfe905fcd2b83
Simson Garfinkel <simsong@acm.org>
2016-02-18 04:29:07 +0000

    added

--- L05/wordcount_top10_2combiner.py
+++ L05/wordcount_top10_2combiner.py
@@ -15,6 +15,7 @@ import heapq
 
 TOPN=10
 class WordCountTopN(MRJob):
+    # Strip the input into <word, 1>
     def mapper(self, _, line):
         for word in line.strip().lower().split():
             yield filter(str.isalpha,word),1
@@ -22,17 +23,26 @@ class WordCountTopN(MRJob):
     def combiner(self, word, counts):
         yield word, sum(counts)
 
-    def reducer(self, word, counts):
-        yield word, sum(counts)
+    # This reducer both sums and maintains a top-N list
+    def reducer_init(self):
+        self.heap = []
 
-    def topN_mapper(self,word,count):
+    def reducer(self, word, counts):
+        heapq.heappush(self.heap,(sum(counts),word))
+        if len(self.heap) > TOPN:
+            heapq.heappop(self.heap)
+
+    def reducer_final(self):
+        for (count,word) in self.heap:
+            yield (word,count)
+
+    # The globalTopN mapper maintains a global topN list.
+    # It uses a single Key to force all values to go through
+    # a single reducer
+    def globalTopN_mapper(self,word,count):
         yield "Top"+str(TOPN), (count,word)
 
-    def topN_combiner(self,_,countsAndWords):
-        for countAndWord in heapq.nlargest(TOPN,countsAndWords):
-            yield _,countAndWord
-
-    def topN_reducer(self,_,countsAndWords):
+    def globalTopN_reducer(self,_,countsAndWords):
         for countAndWord in heapq.nlargest(TOPN,countsAndWords):
             yield _,countAndWord
         
@@ -40,10 +50,13 @@ class WordCountTopN(MRJob):
         return [
             MRStep(mapper=self.mapper,
                    combiner=self.combiner,
-                   reducer=self.reducer),
+                   reducer_init=self.reducer_init,
+                   reducer=self.reducer,
+                   reducer_final=self.reducer_final
+                   ),
 
-            MRStep(mapper=self.topN_mapper,
-                   reducer=self.topN_reducer) ]
+            MRStep(mapper=self.globalTopN_mapper,
+                   reducer=self.globalTopN_reducer) ]
 
 
 
